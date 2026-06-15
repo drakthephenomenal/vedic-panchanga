415524402050e06e8009492ec1626afcf72ee11b
drakthephenomenal <akthephenomenal@zohomail.com>
2026-06-15 19:55:32 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Simple offline cache for Vedic Panchanga
+const CACHE = "vedic-panchanga-v1";
+const ASSETS = ["./", "./index.html", "./app.js", "./style.css", "./manifest.json", "./favicon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  e.respondWith(
+    caches.match(req).then((cached) => {
+      const fetchPromise = fetch(req)
+        .then((res) => {
+          if (res && res.status === 200 && res.type === "basic") {
+            const copy = res.clone();
+            caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
